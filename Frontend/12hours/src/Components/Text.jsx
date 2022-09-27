3e631f33fbb12cd81dc59a6585d732fe4b8451ac
Riel,Jasper J. van <jasper.vanriel@student.fontys.nl>
2022-09-27 15:05:07 +0200

    added componemt

--- Frontend/12hours/src/Components/Text.jsx
+++ Frontend/12hours/src/Components/Text.jsx
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
 class Text extends Component {
-    render() { 
-        return <div><h1>Hello</h1><button>test</button></div>;
+    state = {
+        count: 0
     }
-}
+    handleIncrement = () => {
+        this.setState({
+            count : this.state.count + 1
+        });
+    }
+    render(){
+
+    
+    return(
+        <div>
+            <span>{this.handelState()}</span>
+            <button onClick={this.handleIncrement}>increment</button>
+        </div>
+    );
+    }
+    
+
+    handelState(){
+        const {count} = this.state;
+        return count === 0 ? 'zero' : count;
+    }
+    };
  
 export default Text;
