6a159f51c7b4a3566f8f26d8179606f6919036dd
Riel,Jasper J. van <jasper.vanriel@student.fontys.nl>
2022-09-27 11:16:42 +0200

    Commit React project and test components

---
+++ Frontend/12hours/src/Components/Text.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+class Text extends Component {
+    render() { 
+        return <div><h1>Hello</h1><button>test</button></div>;
+    }
+}
+ 
+export default Text;
