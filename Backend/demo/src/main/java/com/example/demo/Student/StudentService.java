d6f2d803c8dd86097c23d65838412d0298c57b24
Jasper van Riel <222jvr@gmail.com>
2022-10-10 23:01:24 +0200

    commit backend API

---
+++ Backend/demo/src/main/java/com/example/demo/Student/StudentService.java
@@ -0,0 +1,29 @@
+package com.example.demo.Student;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+    public List<Student> GetStudents(){
+        return List.of(new Student(
+                        1L,
+                        "Jasper",
+                        "222jvr@gmai;.com",
+                        LocalDate.of(2003, Month.JUNE,27),
+                        19
+
+                ),
+                new Student(
+                        2L,
+                        "Petra",
+                        "222jvr@gmai;.com",
+                        LocalDate.of(2003, Month.JUNE,27),
+                        19
+
+                ));
+    }
+}
